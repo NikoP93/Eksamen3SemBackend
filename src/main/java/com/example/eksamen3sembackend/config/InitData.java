1a21ea4a39e92efdad8942bc31a6562213209d35
NikoP93 <nipe0001@stud.kea.dk>
2025-01-15 17:25:17 +0100

    First commit

---
+++ src/main/java/com/example/eksamen3sembackend/config/InitData.java
@@ -0,0 +1,14 @@
+package com.example.eksamen3sembackend.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitData implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
---
+++ src/test/java/com/example/eksamen3sembackend/Eksamen3SemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksamen3sembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksamen3SemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
